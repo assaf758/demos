7c931aee0d59e21719bcf9181496e5a9cfdc9dbc
daniels290813 <78727943+daniels290813@users.noreply.github.com>
2024-05-20 15:01:13 +0300

    Update update_demos.sh

--- update_demos.sh
+++ update_demos.sh
@@ -224,7 +224,7 @@ fi
 if [[ "${tag_prefix}" > "1.7" || "${tag_prefix}" == "1.7" ]]; then
     echo "MLRun version >= 1.7 running new update_demos.sh script"
     rm -rf update_demos.sh
-    wget https://raw.githubusercontent.com/mlrun/mlrun/development/automation/scripts/update_demos.sh
+    wget https://raw.githubusercontent.com/daniels290813/mlrun/update_demos_17/automation/scripts/update_demos.sh
     sh update_demos.sh
     error_exit "Done"
 fi
